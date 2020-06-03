49bed36d100790e5f42923b2ed50ccb7eccc4be9
ratuldas6 <53458166+ratuldas6@users.noreply.github.com>
2020-06-03 12:31:36 +0530

    graph produced

--- timeseries.py
+++ timeseries.py
@@ -2,8 +2,26 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
+def divideRow(line):
+    linedata = line.split(';')
+    reqdata = [linedata[3],linedata[4],linedata[5],linedata[6]]
+    return(reqdata)
+
+x=[]
+y=[]
+
 with open('SN_d_hem_V2.0.csv', newline='') as File:  
     reader = csv.reader(File)
     for row in reader:
-        print(row)
+        row_data = divideRow(row[0])
+        x.append(row_data[0])
+        y.append(row_data[1])
 
+plt.plot(x,y, marker='o')
+
+plt.title('Hemispherical Sunspot Numbers (1992-2020)')
+
+plt.xlabel('Time')
+plt.ylabel('Number of sunspots')
+
+plt.show()
